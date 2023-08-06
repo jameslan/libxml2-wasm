0e670de0512ce84a8ba70678a8db14303de754b2
James Lan <jameslan@gmail.com>
2023-08-06 10:27:25 -0700

    Add benchmarks for competing libs

---
+++ benchmark/index.ts
@@ -0,0 +1,23 @@
+import * as benny from 'benny';
+import { readFileSync } from 'fs';
+import * as libxmljs2 from 'libxmljs2';
+import { XMLParser } from 'fast-xml-parser';
+import { XmlDocument } from 'xmldoc';
+
+for (const fixture of ['fixtures/small.xml', 'fixtures/medium.xml', 'fixtures/large.xml']) {
+    const xml = readFileSync(fixture, 'utf-8');
+    benny.suite(
+        `Parsing ${fixture}: ${xml.length} chars`,
+        benny.add('libxmljs2', () => {
+            libxmljs2.parseXmlString(xml);
+        }),
+        benny.add('fast-xml parser', () => {
+            new XMLParser({ ignoreAttributes: false, processEntities: false}).parse(xml)
+        }),
+        benny.add('xmldoc', () => {
+            new XmlDocument(xml);
+        }),
+        benny.cycle(),
+        benny.complete(() => console.log()),
+    )
+}
