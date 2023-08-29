37ca5d3f094acad0064424bc4b1dac3a6bf16a7a
James Lan <jameslan@gmail.com>
2023-08-29 00:00:26 -0700

    Wrapper to export the functions in wasm directly

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,33 +1,33 @@
-import { exec } from 'node:child_process'
-import { TaskFunctionCallback, series } from 'gulp'
-import { mkdirSync, rmSync, promises as fs } from 'node:fs'
-import { relative } from 'node:path'
-import { renderFile } from 'ejs'
+import { exec } from 'node:child_process';
+import { TaskFunctionCallback, series } from 'gulp';
+import { mkdirSync, rmSync, promises as fs } from 'node:fs';
+import { relative } from 'node:path';
+import { renderFile } from 'ejs';
 
-const libxmlSrc = 'libxml2'
-const libxmlBin = 'out'
-const dest = 'lib'
-const libxmlWasm = `${libxmlBin}/libxml2.wasm`
-const libxmlWrapTemplate = 'build/wrap-wasm.ejs'
-const libxmlExportListFile = 'build/wasm-exported.txt'
-const libxmlModuleJs = `${libxmlBin}/libxml2.mjs`
+const libxmlSrc = 'libxml2';
+const libxmlBin = 'out';
+const dest = 'lib';
+const libxmlWasm = `${libxmlBin}/libxml2.wasm`;
+const libxmlWrapTemplate = 'build/wrap-wasm.ejs';
+const libxmlExportListFile = 'build/wasm-exported.txt';
+const libxmlModuleJs = `${libxmlBin}/libxml2.mjs`;
 
 function execInBin(cmd: string[], cb: TaskFunctionCallback) {
-    const subprocess = exec(cmd.join(' '), { cwd: libxmlBin }, cb)
-    subprocess.stdout?.pipe(process.stdout)
-    subprocess.stderr?.pipe(process.stderr)
+    const subprocess = exec(cmd.join(' '), { cwd: libxmlBin }, cb);
+    subprocess.stdout?.pipe(process.stdout);
+    subprocess.stderr?.pipe(process.stderr);
 }
 
 export function clean(cb: TaskFunctionCallback) {
-    rmSync(dest, { recursive: true, force: true })
-    rmSync(libxmlBin, { recursive: true, force: true })
-    cb()
+    rmSync(dest, { recursive: true, force: true });
+    rmSync(libxmlBin, { recursive: true, force: true });
+    cb();
 }
 
 export function init(cb: TaskFunctionCallback) {
-    mkdirSync(dest, { recursive: true })
-    mkdirSync(libxmlBin, { recursive: true })
-    cb()
+    mkdirSync(dest, { recursive: true });
+    mkdirSync(libxmlBin, { recursive: true });
+    cb();
 }
 
 export function configure(cb: TaskFunctionCallback) {
@@ -47,11 +47,11 @@ export function configure(cb: TaskFunctionCallback) {
             '--enable-static',
         ],
         cb,
-    )
+    );
 }
 
 export function compile(cb: TaskFunctionCallback) {
-    execInBin(['emmake', 'make'], cb)
+    execInBin(['emmake', 'make'], cb);
 }
 
 export function link(cb: TaskFunctionCallback) {
@@ -65,18 +65,19 @@ export function link(cb: TaskFunctionCallback) {
             '-s', `EXPORTED_FUNCTIONS=@${relative(libxmlBin, libxmlExportListFile)}`,
         ],
         cb,
-    )
+    );
 }
 
 export async function wrap() {
-    const wasm = (await fs.readFile(libxmlWasm, {})).toString('base64')
+    const wasm = (await fs.readFile(libxmlWasm, {})).toString('base64');
     const exportList = (await fs.readFile(libxmlExportListFile, 'utf-8'))
         .split(/\r?\n/)
         .filter(func => func.trim().length)
-        .map(func => func.slice(1))
-    const src = await renderFile(libxmlWrapTemplate, { wasm, funcs: exportList })
-    await fs.writeFile(libxmlModuleJs, src)
+        .map(func => func.slice(1));
+    const src = await renderFile(libxmlWrapTemplate, { wasm, funcs: exportList });
+    await fs.writeFile(libxmlModuleJs, src);
 }
 
-export const libxml = series(init, configure, compile)
-export const all = series(libxml, link, wrap)
+export const libxml = series(init, configure, compile);
+export const all = series(libxml, link, wrap);
+export const rebuild = series(clean, all);
